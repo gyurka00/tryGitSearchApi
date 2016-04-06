1ef594540fe0358856947c95a6c17e3725c7db09
György Csadó <gyorgy.csado@gmail.com>
2016-04-06 21:18:48 +0200

    create roots and services

--- public/js/app.js
+++ public/js/app.js
@@ -1,40 +1,69 @@
 'use strict';
 
-var chartData = [0,0,0];
-var app = angular.module('gitSearchRepository', ['chart.js']);
-app.controller('searchController', ['$scope', '$http', function($scope, $http) {
+var app = angular.module('gitSearchRepository', ['chart.js', 'ui.router']);
+
+app.service('searchService', function($http) {
   var url = 'https://api.github.com/search/repositories?q=';
-  $scope.search = function(searchKey) {
+  this.getRepositories = function(searchKey, callback) {
     $http.get(url + searchKey).then(function (response) {
-      $scope.repositoriesData = response.data;
-      console.log(response.data);
+      callback(response.data);
     });
   }
-  var urlIssues = 'https://api.github.com/search/issues?q=repo:';
+});
+
+app.service('issuesService', function($http) {
+  var url = 'https://api.github.com/search/issues?q=repo:';
+  this.getIssues = function(repoName, callback) {
+    $http.get(url + repoName).then(function (response) {
+      callback(response.data);
+    });
+  }
+});
+
+app.controller('searchController', ['$scope', '$state', 'searchService', 'issuesService', function($scope, $state, searchService, issuesService) {
+  $scope.search = function(searchKey) {
+    searchService.getRepositories(searchKey, function(data) {
+      $scope.repositoriesData = data;
+    });
+    $state.go('result');
+  }
   $scope.generateStats = function(repo) {
-    chartData = [repo.forks_count, repo.stargazers_count, repo.open_issues_count]
-    $http.get(urlIssues + repo.full_name).then(function (response) {
-      $scope.issuesData = response.data;
-      console.log(response.data);
+    $scope.chartData = [repo.forks_count, repo.stargazers_count, repo.open_issues_count];
+    issuesService.getIssues(repo.full_name, function(data) {
+      $scope.issuesData = data;
+      console.log($scope.issuesData);
     });
   }
 }]);
 
-function getChartData() {
-  return chartData;
-}
-
 app.controller("baseCtrl", function ($scope) {
   $scope.labels = ["Forks count", "Stargazers count", "Open issues count"];
-  // $scope.data = getChartData();
   $scope.type = 'PolarArea';
-
   $scope.getChartData = function() {
-    return getChartData();
+    return $scope.chartData;
   }
-
   $scope.toggle = function () {
     $scope.type = $scope.type === 'PolarArea' ?
       'Pie' : 'PolarArea';
   };
 });
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+      .state('frontpage', {
+        url: '/',
+        templateUrl: './templates/frontpage.html',
+        controller: 'searchController',
+        data: {
+          pageTitle: 'Git Search Repository',
+        },
+      })
+      .state('result', {
+        url: '/result',
+        templateUrl: './templates/result.html',
+        data: {
+          pageTitle: 'Git Search Repository',
+        },
+      })
+  });
